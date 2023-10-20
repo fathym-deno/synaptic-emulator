b9aa721290b36ea4abcb6b255b44caec1d1555c1
mcgear <michael.gearhardt@gmail.com>
2023-10-20 14:00:54 -0600

    rework

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,22 +1,12 @@
 import { Button, Card } from 'flowbite-react';
 import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '../assets/vite.svg';
 
-export default function Home() {
+export default function About() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <Button href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </Button>
-        <Button href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </Button>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React About</h1>
       <Card>
         <Button pill onClick={() => setCount((count) => count + 1)}>
           count is {count}
