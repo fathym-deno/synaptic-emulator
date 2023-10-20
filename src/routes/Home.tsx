548fa6a1b91e2a92b92c0e6d65d842781a415361
mcgear <michael.gearhardt@gmail.com>
2023-10-20 15:08:46 -0600

    Checkpoint

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,7 +11,12 @@ export default function Home() {
         </p>
       </div>
 
-      <Button as={Link} to="/emulator" className="max-w-screen-sm mx-auto mt-8 " gradientDuoTone="purpleToBlue">
+      <Button
+        as={Link}
+        to="/emulator"
+        className="max-w-sm mx-auto mt-8 "
+        gradientDuoTone="purpleToBlue"
+      >
         Get Started Testing your Bot Now
       </Button>
 
