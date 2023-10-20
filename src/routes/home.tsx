d768cff650bf44be48d5e464d3a13e18430280ba
mcgear <michael.gearhardt@gmail.com>
2023-10-20 14:16:39 -0600

    Complete implementation

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,18 +3,46 @@ import { Card } from 'flowbite-react';
 export default function Home() {
   return (
     <>
-      <h1>Welcome to the Synaptic Emulator</h1>
-      <Card>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
+      <div className="text-center mt-10">
+        <h1 className="text-4xl font-bold">Welcome to the Synaptic Emulator</h1>
+        <p className="text-lg mt-4">
           This is an emulator for bots built with the Synaptic bot framework.
         </p>
+      </div>
+      <Card className="mt-10 mx-auto w-3/4">
+        <h2 className="text-2xl font-bold">About Synaptic</h2>
+        <p className="text-lg mt-4">
+          Synaptic is a revolutionary bot building framework that goes beyond
+          mere conversation. Our goal is to create bots that don't just talk,
+          but actually do something meaningful.
+        </p>
+        <p className="text-lg mt-4">
+          Just like the human brain, a bot at its simplest can communicate.
+          However, the true power of the brain lies in the synapses, the
+          connections that link brain activity with the rest of the body,
+          enabling us to perform a multitude of tasks. Similarly, by adding
+          additional functionality to our bots, we can connect these 'synapses'
+          to real, tangible work.
+        </p>
+        <p className="text-lg mt-4">
+          Synaptic is designed with this philosophy in mind. It provides a
+          robust and flexible framework for building bots that can interact,
+          learn, and perform tasks, just like a human would. With Synaptic, you
+          can create bots that not only understand and respond to commands, but
+          also carry out complex tasks, automate processes, and interact with
+          various systems and databases.
+        </p>
+        <p className="text-lg mt-4">
+          The possibilities are endless. Whether you're looking to automate
+          customer service, streamline business processes, or create a virtual
+          assistant, Synaptic provides the tools and infrastructure you need to
+          bring your ideas to life.
+        </p>
+        <p className="text-lg mt-4">
+          Join us in redefining what bots can do. Welcome to the future of bot
+          building with Synaptic.
+        </p>
       </Card>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
