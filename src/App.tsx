38c1fa5629ae2476a6c04435fdba3fd63da8977f
mcgear <michael.gearhardt@gmail.com>
2023-10-20 14:29:27 -0600

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import {
   Route,
 } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './routes/home';
-import About from './routes/about';
+import Home from './routes/Home';
+import About from './routes/About';
 
 export default function App() {
   return (
